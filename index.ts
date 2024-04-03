7a48d522d941c09dece21aa69f4f413befcc951f
M-Waqar-Ahmed <164801039+M-Waqar-Ahmed@users.noreply.github.com>
2024-04-03 16:02:23 +0500

    added a console

--- index.ts
+++ index.ts
@@ -8,8 +8,9 @@ import inquirer from "inquirer";
 
 // 3 compare user input with computer generated number and show result - done
 
-
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+console.log("Welcome to number Guessing Game");
 
 const answers = await inquirer.prompt([
 
